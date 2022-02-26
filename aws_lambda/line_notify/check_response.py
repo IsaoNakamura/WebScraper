9031d86dc6aef2738d1ee04dfc5c9ee9270d2dfc
IsaoNakamura <gariox7@gmail.com>
2022-02-26 15:55:42 +0900

    test.

---
+++ aws_lambda/line_notify/check_response.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+# 実行ファイルのディレクトリ絶対パスを取得
+EXECFILE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# 入力ファイルの格納場所は、実行ファイルディレクトリからの相対パスで指定できる
+SRC_DIR = os.path.join(EXECFILE_DIR, './')
+
+# 自作モジュールの格納場所は、実行ファイルディレクトリからの相対パスで指定できる
+sys.path.append(os.path.join(EXECFILE_DIR, '../../'))
+# internal import
+from lib.util_json import UtilJson
+
+
+if __name__ == '__main__':
+    # エントリポイント
+    try:
+        arguments = sys.argv
+        # - で始まるoption
+        options = [option for option in arguments if option.startswith('-')]
+        if '--resfile' in options:
+            resfile_position = arguments.index('--resfile')
+            resfile = arguments[resfile_position + 1]
+        res = UtilJson.load_json(resfile)
+        if not 'StatusCode' in res:
+            exit(1)
+        if res['StatusCode'] != 200:
+            exit(1)
+        exit(1)
+    except Exception as err:
+        print(err)
+        exit(1)
