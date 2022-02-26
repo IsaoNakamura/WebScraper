41a32aae7260401158eb14fda53c679f0791472c
IsaoNakamura <gariox7@gmail.com>
2022-02-26 14:55:20 +0900

    update.

--- aws_lambda/line_notify/line_notify.py
+++ aws_lambda/line_notify/line_notify.py
@@ -12,4 +12,6 @@ def lambda_handler(event, context):
     headers = {'Authorization': 'Bearer ' + access_token}
     payload = {'message': message}
     res = requests.post(url, headers=headers, params=payload)
+    if res.status_code == 200:
+        exit(1)
     return res.status_code
