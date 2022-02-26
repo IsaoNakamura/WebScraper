b116d5349ed395a42b69700db51678ea7ac3edd9
IsaoNakamura <gariox7@gmail.com>
2022-02-27 04:38:59 +0900

    update.

--- aws_lambda/line_notify/line_notify.py
+++ aws_lambda/line_notify/line_notify.py
@@ -12,6 +12,9 @@ def lambda_handler(event, context):
     headers = {'Authorization': 'Bearer ' + access_token}
     payload = {'message': message}
     res = requests.post(url, headers=headers, params=payload)
-    if res.status_code == 200:
-        exit(1)
-    return res.status_code
+    # return {
+    #     'statusCode': 200,
+    #     'body': json.dumps('Hello from Lambda!')
+    # }
+    # return { 'StatusCode': res.status_code }
+    return json.dumps(res)
