a41d852fb7d6cd5cc4e33a86ee7888904c5470c3
IsaoNakamura <gariox7@gmail.com>
2022-02-27 08:09:40 +0900

    update.

--- aws_lambda/line_notify/gen_payload.py
+++ aws_lambda/line_notify/gen_payload.py
@@ -12,12 +12,10 @@ sys.path.append(os.path.join(EXECFILE_DIR, '../../'))
 # internal import
 from lib.util_json import UtilJson
 
-LINE_ACCESS_TOKEN = "xxxx"
-LINE_END_POINT = "https://notify-api.line.me/api/notify"
+# LINE_END_POINT = "https://notify-api.line.me/api/notify"
 
 PAYLOAD_FORMAT = {
-    "access_token": LINE_ACCESS_TOKEN,
-    "url": LINE_END_POINT,
+    #"url": LINE_END_POINT,
     "message": ""
 }
 
@@ -28,14 +26,10 @@ if __name__ == '__main__':
         arguments = sys.argv
         # - で始まるoption
         options = [option for option in arguments if option.startswith('-')]
-        if '--access_token' in options:
-            access_token_position = arguments.index('--access_token')
-            access_token = arguments[access_token_position + 1]
-            payload['access_token']=access_token
-        if '--url' in options:
-            url_position = arguments.index('--url')
-            url = arguments[url_position + 1]
-            payload['url']=url
+        # if '--url' in options:
+        #     url_position = arguments.index('--url')
+        #     url = arguments[url_position + 1]
+        #     payload['url']=url
         if '--message' in options:
             message_position = arguments.index('--message')
             message = arguments[message_position + 1]
