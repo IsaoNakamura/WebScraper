4850d24b305c9743c547c1e5a29d122272edcb74
IsaoNakamura <gariox7@gmail.com>
2022-02-27 08:05:58 +0900

    apllied crypt by KMS.

--- aws_lambda/line_notify/line_notify.py
+++ aws_lambda/line_notify/line_notify.py
@@ -1,12 +1,25 @@
 import json
+import boto3
+import os
+from base64 import b64decode
 
 # pip3 install 
 import requests
 
 def lambda_handler(event, context):
     # TODO implement
-    access_token = event['access_token']
-    url = event['url']
+    #access_token = event['access_token']
+    access_token_cipher = os.environ.get('LINE_ACCESS_TOKEN')
+    access_token = boto3.client('kms').decrypt(
+        CiphertextBlob=b64decode(access_token_cipher),
+        EncryptionContext={'LambdaFunctionName': os.environ['AWS_LAMBDA_FUNCTION_NAME']}
+    )['Plaintext'].decode('utf-8')
+    #url = event['url']
+    url_cipher = os.environ.get('LINE_ENDPOINT')
+    url = boto3.client('kms').decrypt(
+        CiphertextBlob=b64decode(url_cipher),
+        EncryptionContext={'LambdaFunctionName': os.environ['AWS_LAMBDA_FUNCTION_NAME']}
+    )['Plaintext'].decode('utf-8')
     message = event['message']
     print(message)
     headers = {'Authorization': 'Bearer ' + access_token}
